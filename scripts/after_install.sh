676da71aef9b40c825da5eaf0801a402a3b61230
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2022-08-12 16:10:00 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,9 @@ chown -R ubuntu:ubuntu /home/ubuntu/nodejs/
 echo  "***Installing npm package ***" >> /tmp/nodejs-api-prod_deploy_logs
 echo >> /tmp/nodejs-api-prod_deploys_logs
 runuser -l ubuntu -c 'cd /home/ubuntu/nodejs && npm install'
-runuser -l ubuntu -c 'cd /home/ubuntu/nodejs && npm start'
-#runuser -l ubuntu -c 'cd /home/ubuntu/nodejs && npm install --unsafe-perm'
+runuser -l ubuntu -c 'cd /home/ubuntu/nodejs && npm install --unsafe-perm'
 #runuser -l ubuntu -c 'cd /home/ubuntu/nodejs && npm run build'
+sleep 10
+echo "***starting nodejs-api-prod application ***" >> /tmp/nodejs-api-prod_deploy_logs
+runuser -l ubuntu -c 'cd /home/ubuntu/nodejs && sudo npm start server/server.js ' >> /tmp/nodejs-api-prod_deploy_logs
+
