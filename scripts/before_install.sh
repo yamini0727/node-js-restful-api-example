d0bfd91039471a2b4ee50b65995851c4d5e90f19
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2022-11-14 13:20:34 +0530

    Delete before_install.sh

--- scripts/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-echo "** nodejs-api-prod process status **" >> /tmp/nodejs-api-prod_deploy_logs
-
-rm -rf /home/ubuntu/nodejs
-
-if [ ! -d /home/ubuntu/nodejs ]; then
-runuser -l ubuntu -c 'mkdir -p /home/ubuntu/nodejs' >> /tmp/nodejs-prod_deploy_logs
-fi
