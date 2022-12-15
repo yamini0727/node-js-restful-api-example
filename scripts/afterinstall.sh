5481e1daf3690b235dce15e89a0a3fc07753c8f8
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2022-12-15 14:04:20 +0530

    Update afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
-chown -R ec2-user:ec2-user /opt/elasticbeanstalk/hooks/appdeploy/pre
+chown -R ec2-user:ec2-user /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend
 
 echo  "***Installing npm package ***" >> /tmp/eb-backend-api-prod_deploy_logs
 echo >> /tmp/eb-backend-api-prod_deploys_logs
-runuser -l ec2-user -c 'cd /home/ec2-user/eb-backend && npm install'
-runuser -l ec2-user -c 'cd /home/ec2-user/eb-backend && npm install --unsafe-perm'
-runuser -l ec2-user -c 'cd /home/ec2-user/eb-backend && npm run build'
-runuser -l ec2-user -c 'cd /home/ec2-user/eb-backend && npm start'
+runuser -l ec2-user -c 'cd /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend && npm install'
+runuser -l ec2-user -c 'cd /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend && npm install --unsafe-perm'
+runuser -l ec2-user -c 'cd /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend && npm run build'
+runuser -l ec2-user -c 'cd /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend && npm start'
 then
 echo "BUILD SUCCESSFUL" >> /tmp/eb-backend-api-prod_deploy_logs
 echo >> /tmp/eb-backend-api-prod_deploy_logs
