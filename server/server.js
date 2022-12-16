965c1c70cd53ed27df62eb121c3f3d9ba907e35a
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2022-12-16 15:55:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var morgan = require('morgan');
 var eventsRouter = require('./routers/events-router');
 
 var app = express();
-var port = 3000;
+var port = 8080;
 
 app.use(morgan('dev'));
 app.use(express.static('client'));
