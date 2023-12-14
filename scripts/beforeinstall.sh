3a3c23d0a81a6b363320e5e3e7e603e5702d9e40
yamini0727 <81848316+yamini0727@users.noreply.github.com>
2023-12-14 16:18:25 +0530

    Delete scripts directory

--- scripts/beforeinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-echo "** eb-backend-chat-api-prod process status **" >> /tmp/eb-backend-chat-api-prod_deploy_logs
-
-rm -rf /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend
-
-if [ ! -d /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend ]; then
-runuser -l ec2-user -c 'mkdir -p /opt/elasticbeanstalk/hooks/appdeploy/pre/eb-backend' >> /tmp/eb-backend-chat-prod_deploy_logs
-fi
